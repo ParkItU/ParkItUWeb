d7c5e40be9955b8768b94add501303f523b11581
dudubarbaro <dudubarbaro>
2023-09-26 14:01:38 -0300

    feat: telas de sobre e ajuda

--- src/views/AjudaView.vue
+++ src/views/AjudaView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBarComp from '../components/NavBarComp.vue'
+import AjudaComp from '../components/AjudaComp.vue'
 </script>
 
 <template>
   <main>
     <NavBarComp />
+    <AjudaComp />
   </main>
 </template>
--- src/views/SobreView.vue
+++ src/views/SobreView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBarComp from '../components/NavBarComp.vue'
+import SobreComp from '../components/SobreComp.vue'
 </script>
 
 <template>
   <main>
     <NavBarComp />
+    <SobreComp />
   </main>
 </template>
