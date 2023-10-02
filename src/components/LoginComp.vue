b92d695b9cf37ecd26a6ff7a0cf2ea9280fd1630
BrunnoDC <>
2023-10-02 10:54:06 -0300

    feat: login final

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -1,20 +1,20 @@
 <template>
   <!-- component -->
   <div class="bg-white dark:bg-gray-900">
-    <div class="flex justify-center h-screen">
+    <div class="flex h-screen">
       <div
-        class="hidden bg-cover lg:block lg:w-2/3"
+        class="hidden bg-cover lg:block lg:w-3/3"
         style="
-          background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80);
+          background-image: url(https://media.istockphoto.com/id/636444558/pt/foto/vazio-estacionamento-lotes-vista-a%C3%A9rea.jpg?s=612x612&w=0&k=20&c=zhhTe_I5mf_kXAeoB-yKHH_JNQKmBttbRNDRbzbHg2c=);
         "
       >
         <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
           <div>
-            <h2 class="text-4xl font-bold text-white">Brand</h2>
+            <h2 class="text-4xl font-bold text-white"> A procura por estacionamento em sua região aumentou?</h2>
 
-            <p class="max-w-xl mt-3 text-gray-300">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla
-              laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae
+            <p class="max-w-xl mt-3 text-gray-300 font-bold" >
+              Junte-se a nós! Contrate a ParkItU.
+               
             </p>
           </div>
         </div>
@@ -23,22 +23,22 @@
       <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
         <div class="flex-1">
           <div class="text-center">
-            <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Brand</h2>
+            <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Login</h2>
 
-            <p class="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+            <p class="mt-3 text-gray-500 dark:text-gray-300">Faça login para acessar sua conta</p>
           </div>
 
           <div class="mt-8">
             <form>
               <div>
                 <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
-                  >Email Address</label
+                  >Insira seu e-mail</label
                 >
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  placeholder="example@example.com"
+                  placeholder="exemplo@exemplo.com"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
@@ -46,12 +46,12 @@
               <div class="mt-6">
                 <div class="flex justify-between mb-2">
                   <label for="password" class="text-sm text-gray-600 dark:text-gray-200"
-                    >Password</label
+                    >Insira sua senha</label
                   >
                   <a
                     href="#"
                     class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline"
-                    >Forgot password?</a
+                    >Esqueceu sua senha?</a
                   >
                 </div>
 
@@ -59,7 +59,7 @@
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="Your Password"
+                  placeholder="*******"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
@@ -68,15 +68,15 @@
                 <button
                   class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
                 >
-                  Sign in
+                  Logar
                 </button>
               </div>
             </form>
 
             <p class="mt-6 text-sm text-center text-gray-400">
-              Don&#x27;t have an account yet?
+              Não tem uma conta ainda? 
               <a href="#" class="text-blue-500 focus:outline-none focus:underline hover:underline"
-                >Sign up</a
+                >Cadastre-se</a
               >.
             </p>
           </div>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,11 @@
 <script setup>
 import LoginComp from '../components/LoginComp.vue'
-import FooterComp from '../components/FooterComp.vue'
+
 </script>
 
 <template>
   <main>
     <LoginComp />
-    <FooterComp />
+    
   </main>
 </template>
