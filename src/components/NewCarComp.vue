3b769e99b3dc64c01490781ba98368939e44a8ce
dudubarbaro <dudubarbaro>
2023-10-02 10:54:44 -0300

    feat: frontend de cadastro de carros finalizado

--- src/components/NewCarComp.vue
+++ src/components/NewCarComp.vue
@@ -1,10 +1,8 @@
-<!-- component -->
 <template>
-  <!-- component -->
-  <div class="flex items-center justify-center p-12">
-    <!-- Author: FormBold Team -->
-    <!-- Learn More: https://formbold.com -->
+  <div class="flex items-center justify-center">
     <div class="mx-auto w-full max-w-[550px]">
+      <h1 class="text-4xl font-bold text-center text-gray-900 dark:text-white">Cadastro</h1>
+      <br />
       <form action="" method="">
         <div class="-mx-3 flex flex-wrap">
           <div class="w-full px-3 sm:w-1/2">
@@ -54,7 +52,7 @@
           <div class="w-full px-3 sm:w-1/2">
             <div class="mb-5">
               <label for="date" class="mb-3 block text-base font-medium text-[#07074D]">
-                Date
+                Data
               </label>
               <input
                 type="date"
@@ -67,7 +65,7 @@
           <div class="w-full px-3 sm:w-1/2">
             <div class="mb-5">
               <label for="time" class="mb-3 block text-base font-medium text-[#07074D]">
-                Time
+                Hora
               </label>
               <input
                 type="time"
@@ -83,57 +81,79 @@
           <label class="mb-3 block text-base font-medium text-[#07074D]"> Adicionar Foto? </label>
           <div class="flex items-center space-x-6">
             <div class="flex items-center">
-              <input type="radio" name="radio1" id="radioButton1" class="h-5 w-5" />
+              <input
+                type="radio"
+                name="radio1"
+                id="radioButton1"
+                class="h-5 w-5"
+                v-model="addPhoto"
+                value="sim"
+              />
               <label for="radioButton1" class="pl-3 text-base font-medium text-[#07074D]">
                 Sim
               </label>
             </div>
             <div class="flex items-center">
-              <input type="radio" name="radio1" id="radioButton2" class="h-5 w-5" />
+              <input
+                type="radio"
+                name="radio1"
+                id="radioButton2"
+                class="h-5 w-5"
+                v-model="addPhoto"
+                value="nao"
+              />
               <label for="radioButton2" class="pl-3 text-base font-medium text-[#07074D]">
                 Não
               </label>
             </div>
           </div>
         </div>
-        <div class="flex items-center justify-center p-12">
-          <div class="mx-auto w-full max-w-[550px] bg-white">
-            <form class="py-6 px-9" action="" method="">
-              <div class="mb-6 pt-4">
-                <label class="mb-5 block text-xl font-semibold text-[#07074D]"> Upload File </label>
 
-                <div class="mb-8">
-                  <input type="file" name="file" id="file" class="sr-only" />
-                  <label
-                    for="file"
-                    class="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
+        <div v-if="addPhoto === 'sim'">
+          <!-- Seção de Upload de Fotos -->
+          <div class="mb-6 pt-4">
+            <label class="mb-5 block text-xl font-semibold text-[#07074D]"> Upload File </label>
+
+            <div class="mb-8">
+              <input type="file" name="file" id="file" class="sr-only" />
+              <label
+                for="file"
+                class="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
+              >
+                <div>
+                  <span class="mb-2 block text-xl font-semibold text-[#07074D]">
+                    Drop files here
+                  </span>
+                  <span class="mb-2 block text-base font-medium text-[#6B7280]"> Or </span>
+                  <span
+                    class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]"
                   >
-                    <div>
-                      <span class="mb-2 block text-xl font-semibold text-[#07074D]">
-                        Drop files here
-                      </span>
-                      <span class="mb-2 block text-base font-medium text-[#6B7280]"> Or </span>
-                      <span
-                        class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]"
-                      >
-                        Browse
-                      </span>
-                    </div>
-                  </label>
+                    Browse
+                  </span>
                 </div>
-              </div>
-
-              <div>
-                <button
-                  class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-                >
-                  Criar
-                </button>
-              </div>
-            </form>
+              </label>
+            </div>
           </div>
+        </div>
+
+        <div>
+          <button
+            class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
+          >
+            Criar
+          </button>
         </div>
       </form>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      addPhoto: 'nao'
+    }
+  }
+}
+</script>
