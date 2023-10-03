38a8963d9acb35668f64e54146df3d9ad9d51dfb
BrunnoDC <>
2023-10-03 15:26:02 -0300

    sobre final

--- src/views/SobreView.vue
+++ src/views/SobreView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NavBarComp from '../components/NavBarComp.vue'
 import SobreComp from '../components/SobreComp.vue'
+import FooterComp from '../components/FooterComp.vue'
 </script>
 
 <template>
   <main>
     <NavBarComp />
     <SobreComp />
+    <FooterComp />
   </main>
 </template>
