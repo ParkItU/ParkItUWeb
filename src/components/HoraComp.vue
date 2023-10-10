bd61cc526a37cde8f8ec681a0122a8b2d2564fb7
BrunnoDC <>
2023-10-10 16:37:15 -0300

    page hora inicial

--- src/components/HoraComp.vue
+++ src/components/HoraComp.vue
@@ -1,4 +1,32 @@
 <template>
-
+<!-- component -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iniciar sesión - POC WCS</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+      <div class="flex justify-center mb-8"> 
+        <img src="https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png" alt="Logo" class="w-30 h-20">
+      </div>
+      <h1 class="text-2xl font-semibold text-center text-gray-500 mt-8 mb-6">¡Bienvenido a Fideliza Más!</h1> 
+      <p class="text-sm text-gray-600 text-justify mt-8 mb-6">Al unirte a nuestro programa de fidelización y tokenización de puntos, conseguirás obtener puntos con cada compra y canjearlos por descuentos exclusivos, regalos especiales o convertirlos en criptomonedas.</p> 
+      <div class="flex justify-center space-x-4 my-4">
+        <button class="bg-gradient-to-r from-cyan-400 to-cyan-600 text-white px-4 py-2 rounded-md w-1/3">Comercio</button>
+        <button class="bg-gradient-to-r from-cyan-400 to-cyan-600 text-white px-4 py-2 rounded-md w-1/3">Usuario</button>
+      </div>
+      <div class="text-center">
+        <p class="text-sm">Volver a <a href="#" class="text-cyan-600">Iniciar sesión</a></p>
+      </div>
+      <p class="text-xs text-gray-600 text-center mt-8">&copy; 2023 WCS LAT</p>
+    </div>
+  </div>
+</body>
+</html>
     
 </template>
