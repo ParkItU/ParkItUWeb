33da7e75c12a4ea60ab53ec0a2c7d9bee26175c8
dudubarbaro <dudubarbaro>
2023-11-14 20:37:53 -0300

    Refactor input fields and add save function to CadastroGaragemComp.vue component

--- src/components/CadastroGaragemComp.vue
+++ src/components/CadastroGaragemComp.vue
@@ -12,14 +12,8 @@
               <label for="name" class="mb-3 flex text-base font-medium text-[#07074D]">
                 Nome da Garagem
               </label>
-              <input
-                v-model="currentGarage.nameGarage"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Digite aqui"
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
+              <input v-model="currentGarage.nameGarage" type="text" name="name" id="name" placeholder="Digite aqui"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
           </div>
           <div class="w-full px-3 sm:w-1/2">
@@ -27,25 +21,19 @@
               <label for="endereco" class="mb-3 flex text-base font-medium text-[#07074D]">
                 Endereço
               </label>
-              <input
-                v-model="currentGarage.adressGarage"
-                type="text"
-                name="endereco"
-                id="endereco"
+              <input v-model="currentGarage.adressGarage" type="text" name="endereco" id="endereco"
                 placeholder="Digite Aqui"
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
           </div>
         </div>
 
         <div>
-          <button
-            type="submit"
-            class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-          >
+          <button type="submit" @click="save"
+            class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
             Adicionar
           </button>
+
         </div>
       </form>
     </div>
@@ -53,49 +41,37 @@
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
-import axios from 'axios'
-
-const currentGarage = reactive({
-  nameGarage: '',
-  adressGarage: ''
-})
-
-const addPhoto = ref('nao')
-const downloadURL = ref(null)
-
-async function save() {
-  try {
-    // Envie os dados da garagem para a API
-    const response = await axios.post(
-      'https://backendparkitu-dev.fl0.io/api/garages/',
-      currentGarage
-    )
-    console.log('Garagem cadastrada com sucesso:', response.data)
-
-    // Limpe os campos do formulário
-    Object.assign(currentGarage, {
-      nameGarage: '',
-      addressGarage: ''
-    })
-
-    // Redirecione para a rota /garagens
-    this.$router.push('/garages')
-  } catch (error) {
-    console.error('Erro ao cadastrar a garagem:', error)
-  }
-}
+import { reactive } from 'vue';
+import axios from 'axios';
 
 export default {
   data() {
     return {
-      currentGarage,
-      addPhoto,
-      downloadURL
-    }
+      currentGarage: {
+        nameGarage: '',
+        adressGarage: '',
+      },
+    };
   },
   methods: {
-    save
-  }
-}
+    async save() {
+      try {
+        // Send garage data to the API
+        const response = await axios.post('https://backendparkitu-dev.fl0.io/api/garages/', this.currentGarage);
+        console.log('Garagem cadastrada com sucesso:', response.data);
+
+        // Clear form fields
+        this.currentGarage = {
+          nameGarage: '',
+          adressGarage: '',
+        };
+
+        // Redirect to the /garages route
+        this.$router.push('/garages');
+      } catch (error) {
+        console.error('Erro ao cadastrar a garagem:', error);
+      }
+    },
+  },
+};
 </script>
--- src/components/NewCarComp.vue
+++ src/components/NewCarComp.vue
@@ -1,3 +1,4 @@
+<!-- YourComponent.vue -->
 <template>
   <div class="flex items-center justify-center">
     <div class="mx-auto w-full max-w-[550px]">
@@ -12,14 +13,8 @@
               <label for="fName" class="mb-3 flex text-base font-medium text-[#07074D]">
                 Nome do Carro
               </label>
-              <input
-                v-model="currentCar.carName"
-                type="text"
-                name="fName"
-                id="fName"
-                placeholder="Digite aqui"
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
+              <input v-model="currentCar.carName" type="text" name="fName" id="fName" placeholder="Digite aqui"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
           </div>
           <div class="w-full px-3 sm:w-1/2">
@@ -27,14 +22,8 @@
               <label for="lName" class="mb-3 flex text-base font-medium text-[#07074D]">
                 Nome do Dono do Carro
               </label>
-              <input
-                v-model="currentCar.carOwner"
-                type="text"
-                name="lName"
-                id="lName"
-                placeholder="Digite Aqui"
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
+              <input v-model="currentCar.carOwner" type="text" name="lName" id="lName" placeholder="Digite Aqui"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
           </div>
         </div>
@@ -42,29 +31,17 @@
           <label for="guest" class="mb-3 flex text-base font-medium text-[#07074D]">
             Placa do Carro
           </label>
-          <input
-            v-model="currentCar.licensePlate"
-            type="text"
-            name="guest"
-            id="guest"
-            placeholder="XXXXYYYY"
-            min="0"
-            class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          />
+          <input v-model="currentCar.licensePlate" type="text" name="guest" id="guest" placeholder="XXXXYYYY" min="0"
+            class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
         </div>
 
         <div class="mb-5">
           <label for="carOwnerPhone" class="mb-3 flex text-base font-medium text-[#07074D]">
             Número de Telefone
           </label>
-          <input
-            v-model="currentCar.carOwnerPhone"
-            type="text"
-            name="carOwnerPhone"
-            id="carOwnerPhone"
+          <input v-model="currentCar.carOwnerPhone" type="text" name="carOwnerPhone" id="carOwnerPhone"
             placeholder="999999999"
-            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          />
+            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
         </div>
         <div class="-mx-3 flex flex-wrap">
           <div class="w-full px-3 sm:w-1/2">
@@ -72,13 +49,8 @@
               <label for="date" class="mb-3 flex text-base font-medium text-[#07074D]">
                 Data
               </label>
-              <input
-                v-model="currentCar.dateTime"
-                type="date"
-                name="date"
-                id="date"
-                class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
+              <input v-model="currentCar.dateTime" type="date" name="date" id="date"
+                class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
           </div>
           <div class="w-full px-3 sm:w-1/2">
@@ -86,12 +58,8 @@
               <label for="garage" class="mb-3 flex text-base font-medium text-[#07074D]">
                 Escolha a Garagem
               </label>
-              <select
-                v-model="currentCar.garageId"
-                name="garage"
-                id="garage"
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              >
+              <select v-model="currentCar.garageId" name="garage" id="garage"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
                 <option v-for="garage in garages" :key="garage.id" :value="garage.id">
                   {{ garage.nameGarage }}
                 </option>
@@ -101,11 +69,11 @@
         </div>
 
         <div>
-          <button
-            type="submit"
+          <button type="submit" :class="{ 'cursor-not-allowed opacity-50': isLoading }"
             class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-          >
-            Adicionar
+            :disabled="isLoading">
+            <span v-if="isLoading">Adicionando...</span>
+            <span v-else>Adicionar</span>
           </button>
         </div>
       </form>
@@ -114,9 +82,13 @@
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from 'vue'
-import axios from 'axios'
-import garageService from '@/services/garages.js'
+import { ref, reactive, onMounted } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+import garageService from '@/services/garages.js';
+
+const isLoading = ref(false);
+const router = useRouter();
 
 const currentCar = reactive({
   carName: '',
@@ -124,31 +96,43 @@ const currentCar = reactive({
   licensePlate: '',
   dateTime: '',
   carOwnerPhone: '',
-  garageId: null
-})
+  garageId: null,
+});
 
-const garages = ref([])
+const garages = ref([]);
 
 onMounted(async () => {
-  const data = await garageService.getAllGarages()
-  garages.value = data
-})
+  try {
+    isLoading.value = true;
+    const data = await garageService.getAllGarages();
+    garages.value = data;
+  } catch (error) {
+    console.error('Error fetching garages:', error);
+    // Handle the error, e.g., display an error message to the user
+  } finally {
+    isLoading.value = false;
+  }
+});
 
 async function save() {
   try {
-    const response = await axios.post('https://backendparkitu-dev.fl0.io/api/cars/', currentCar)
-    console.log('Carro cadastrado com sucesso:', response.data)
+    isLoading.value = true;
+    const response = await axios.post('https://backendparkitu-dev.fl0.io/api/cars/', currentCar);
+    console.log('Carro cadastrado com sucesso:', response.data);
     Object.assign(currentCar, {
       carName: '',
       carOwner: '',
       licensePlate: '',
       dateTime: '',
       carOwnerPhone: '',
-      garageId: null
-    })
-    this.$router.push('/cars')
+      garageId: null,
+    });
+    router.push('/cars');
   } catch (error) {
-    console.error('Erro ao cadastrar o carro:', error)
+    console.error('Erro ao cadastrar o carro:', error);
+    // Add code to handle errors, like displaying an error message to the user
+  } finally {
+    isLoading.value = false;
   }
 }
 </script>
